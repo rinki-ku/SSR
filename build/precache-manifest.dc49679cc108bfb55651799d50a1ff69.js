869f43f9f5fa6aed1ce7a2c70db3c52a2e2e1e42
rinki-terrera <rinki.kumari@terrera.ag>
2019-12-21 14:46:20 +0100

    server side rendering app implemetation

---
+++ build/precache-manifest.dc49679cc108bfb55651799d50a1ff69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f6ed3054223ccaa148b092550b2c52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "062b1d599188bdba2d14",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c953d10db96a0d6a528b",
+    "url": "/static/js/2.efa09b16.chunk.js"
+  },
+  {
+    "revision": "062b1d599188bdba2d14",
+    "url": "/static/js/main.ac04dbea.chunk.js"
+  },
+  {
+    "revision": "46c7099f85dc07c6500d",
+    "url": "/static/js/runtime~main.3c502ce5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
